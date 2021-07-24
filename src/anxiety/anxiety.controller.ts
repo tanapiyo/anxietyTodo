d51897abad59ad318740ce41c6f4420c77d01c05
tanapiyo <t1820018@edu.cc.uec.ac.jp>
2021-07-24 12:46:44 +0900

    add test controller and service

--- src/anxiety/anxiety.controller.ts
+++ src/anxiety/anxiety.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { AnxietyService } from './anxiety.service';
 
 @Controller('anxiety')
-export class AnxietyController {}
+export class AnxietyController {
+  constructor(private readonly anxietyService: AnxietyService) {}
+
+  @Get()
+  getTest(): string {
+    return this.anxietyService.getTest();
+  }
+}
--- src/anxiety/anxiety.service.ts
+++ src/anxiety/anxiety.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AnxietyService {}
+export class AnxietyService {
+  getTest(): string {
+    return 'hi';
+  }
+}
